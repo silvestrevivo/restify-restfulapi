b57d642b6103832620b08d19bab05b36bcfa04f3
silvestrevivo <silvestrevivomillan@gmail.com>
2018-10-31 10:30:40 +0100

    adding and retrieving costumers from api

--- routes/customers.js
+++ routes/customers.js
@@ -1,10 +1,41 @@
 'use strict'
 
 const errors = require('restify-errors')
+const Customer = require('../models/Customer')
 
 module.exports = server => {
-  server.get('/customers', (req, res, next) => {
-    res.send({ message: 'test' })
-    next()
+  // Get Customers
+  server.get('/customers', async (req, res, next) => {
+    try {
+      const customer = await Customer.find({})
+      res.send(customer)
+      next()
+    } catch (error) {
+      return next(new errors.InvalidContentError(err))
+    }
+  })
+
+  // Add Customer
+  server.post('/customers', async (req, res, next) => {
+    // Check for JSON
+    if (!req.is('application/json')) {
+      return next(new errors.InvalidContentError(`Expects 'application/json' `))
+    }
+
+    const { name, email, balance } = req.body
+
+    const customer = new Customer({
+      name,
+      email,
+      balance,
+    })
+
+    try {
+      const newcCustomer = await customer.save()
+      res.send(201)
+      next()
+    } catch (error) {
+      return next(new errors.InternalError(err.message))
+    }
   })
 }
